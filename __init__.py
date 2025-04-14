c987efe980711f7f35c49ca6aad062e8feba61bf
Charles Weiss <69219836+weisscharlesj@users.noreply.github.com>
2025-04-13 20:19:28 -0500

    Added pytest tests and from_atoms method of generating reducibles

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from .grouptheory import get_header, get_mulliken, Symmetry, gen_reducible, get_reducible_atom
+from .charactertables import tables, headers, mulliken, rot_trans_modes, IR_active, Raman_active, masks, atom_contribution
