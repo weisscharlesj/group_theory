ea06df5c691ac8f9981de95bcd5b6d43a0edad9b
Charles Weiss <charles.weiss@augie.edu>
2025-08-14 16:17:14 -0500

    Updated atom contributions data and tests
    Corrected typos on atom contributions data table and added more tests to generating reducible representations based on stationary atoms.

--- tests/test_representations.py
+++ tests/test_representations.py
@@ -5,14 +5,15 @@ import numpy as np
 from ..representations import Reducible
 
 # Test data with (reducible representation, num of irreducibles, point group)
-test_data = (
-            ((2), (2), 'c1'),
+reducible_data = (
             ((2, 0), (1, 1), 'cs'),
             ((3, 1), (2, 1), 'ci'),
+            ((2), (2), 'c1'),
             ((3, 1), (2, 1), 'c2'),
             ((3, 0, 0), (1, 1), 'C3'),
             ((4, 0, 0, 0), (1, 1, 1), 'c4'),
             ((21, 1, 1, 1, 1), (5, 4, 4), 'c5'),
+            ((6, 0, 0, 0, 0, 0), (1, 1, 1, 1), 'c6'),
             ([9, -1, 3, 1], [3, 1, 3, 2], 'c2v'),
             ([3, 0, 1], (1, 0, 1), 'c3v'),
             ((6, 0, 2, 0, 0), (1, 1, 1, 1, 1), 'c4v'),
@@ -20,21 +21,40 @@ test_data = (
             ((15, 0, 0, 7, -2, -2), (3, 4, 2, 1), 'c3h'),
             ((5, -1, 1, -1, -1, 1, -5, 1), (0, 0, 1, 1, 2, 0), 'c4h'),
             ((4, 0, 0, 0), (1, 1, 1, 1), 'd2'),
-            ([4, 1, 0, 0, 2], (1, 0, 0, 0, 1), 'Td'),
-            ((4, 0, 0, 2, 0, 0, 0, 4, 2, 0), (1, 0, 1, 0, 0, 0, 0, 0, 0, 1), 'd4h'),
+            ((5, -1, 1, 3, -1), (1, 0, 2, 0, 1), 'd2d'),
             ((5, 2, 1, 3, 0, 3), (2, 0, 1, 0, 1, 0), 'd3h'),
+            ((4, 0, 0, 2, 0, 0, 0, 4, 2, 0), (1, 0, 1, 0, 0, 0, 0, 0, 0, 1), 'd4h'),
             ((6, 0, 0, 0, -2, 0, 0, 0, 0, -6, 0, 2), (0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1), 'd6h'),
+            ([4, 1, 0, 0, 2], (1, 0, 0, 0, 1), 'Td'),
+            ([8, -1, 4, 0, -2], (0, 1, 2, 1, 0), 'td'),
             ((6, 0, 0, 2, 2, 0, 0, 0, 4, 2), (1, 0, 1, 0, 0, 0, 0, 0, 1, 0), 'Oh'),
             ((7, 1, 1, 3, 3, 1, 1, 1, 5, 3), (2, 0, 1, 0, 0, 0, 0, 0, 1, 0), 'oh'),
-            ((6, 0, 0, 0, 0, 0), (1, 1, 1, 1), 'c6')
             )
 
 
-@pytest.mark.parametrize('gamma, true_n_irred, group', test_data)
+@pytest.mark.parametrize('gamma, true_n_irred, group', reducible_data)
 def test_decomp(gamma, true_n_irred, group):
     calc_n_irred = Reducible(gamma, group).decomp()
 
     assert np.all(true_n_irred == calc_n_irred)
+
+all_motion_data = (
+    ((6, 0, 0, 6), (18, 0, 0, 6), 'c2h'),
+    ((3, 1, 3, 1), (9, -1, 3, 1), 'c2v'),
+    ((4, 1, 2), (12, 0, 2), 'c3v'),
+    ((6, 2, 2, 4, 2), (18, 2, -2, 4, 2), 'c4v'),
+    ((8, 2, 0, 0, 0, 4), (24, 0, 0, 0, 0, 4), 'd3d'),
+    ((6, 3, 2, 4, 1, 4), (18, 0, -2, 4, -2, 4), 'd3h'),
+    ((5, 1, 1, 3, 1, 1, 1, 5, 3, 1), (15, 1, -1, -3, -1, -3, -1, 5, 3, 1), 'd4h'),
+    ((5, 2, 1, 1, 3), (15, 0, -1, -1, 3), 'td'),
+    ((7, 1, 1, 3, 3, 1, 1, 1, 5, 3), (21, 0, -1, 3, -3, -3, -1, 0, 5, 3), 'oh')
+    )
+
+@pytest.mark.parametrize('stationary_atoms, reducible_rep, group', all_motion_data)
+def test_from_atoms(stationary_atoms, reducible_rep, group):
+
+    assert np.all(Reducible.from_atoms(stationary_atoms, group).gamma ==
+                  np.array(reducible_rep))
 
 
 class Test_ReducibleMethods():
@@ -88,15 +108,6 @@ class Test_ReducibleMethods():
         assert np.all(tDCE.raman_active(to_dict=True) ==
                       {'Ag': 5, 'Bg': 1, 'Au': 0, 'Bu': 0})
 
-    def test_from_atoms(self):
-        assert np.all(Reducible.from_atoms([6, 0, 0, 6], 'c2h').gamma ==
-                      np.array([18, 0, 0, 6]))
-
-        assert np.all(Reducible.from_atoms([3, 1, 3, 1], 'c2v').gamma ==
-                      np.array([9, -1, 3, 1]))
-
-        assert np.all(Reducible.from_atoms([21, 1, 1, 1, 1, 1, 1, 5], 'd5h').
-                      gamma == np.array([63, 3, -1, -1, 1, 1, -2, 5]))
 
     def test_from_irred(self):
         test_rep = Reducible.from_irred([1, 0, 1, 0], 'c2v')
